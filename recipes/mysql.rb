bb7c5f2781392d3cb641639971c4472aef1d5c88
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-04-04 10:56:59 -0500

    updating gem install

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -26,7 +26,7 @@ unless File.exists?(installed_file)
     include_recipe "apt"
   end
 
-  include_recipe "mysql::ruby"
+  include_recipe "mysql-chef_gem"
 
   my_cnf =  if platform?('centos', 'redhat')
                 "/etc"

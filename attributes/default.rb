6be65d762bca155277b3913ea98c00529f6ee366
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-04-11 09:57:54 -0500

    Bumping version of magento to install

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 # General settings
-default[:magento][:download_url] = "http://www.magentocommerce.com/downloads/assets/1.8.0.0/magento-1.8.0.0.tar.gz"
+default[:magento][:download_url] = 'http://www.magentocommerce.com/downloads/assets/1.8.1.0/magento-1.8.1.0.tar.gz'
 default[:magento][:dir] = "/var/www/magento"
 default[:magento][:use_sample_data] = false
 default[:magento][:sample_data_url] = 'http://www.magentocommerce.com/downloads/assets/1.6.1.0/magento-sample-data-1.6.1.0.tar.gz'
